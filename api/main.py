ce568886f9079e02f761ee20bcc2c7e2f6ac250f
Suvansh7 <jindalsuvansh7@gmail.com>
2024-08-13 16:48:34 +0530

    final

--- api/main.py
+++ api/main.py
@@ -1,13 +1,40 @@
 from fastapi import FastAPI
 
-
-
 app = FastAPI()
 
-@app.get("/text")
+def next_question(ans):
+    corpus = corpus + ans
+
+    import google.generativeai as genai
+    GOOGLE_API_KEY="AIzaSyD-WsKQ2O-isAK-PJjFxTusl1-TxcQ8l2E"
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel('gemini-1.5-flash')
+
+    p=f'''
+
+        Act Like a Interviewer and ask question which should be revelant to following answer -
+        {ans}
+
+        Make sure you ask questions which must be relevant to {corpus}
+
+    '''
+
+    response = model.generate_content(p)
+    ques = response.text
+    return ques
+    
+
+@app.get("/")
+def display():
+
+    first_question ="Tell Me about Yourself ?"
+
+    return first_question
+
+@app.get("/answering")
 def display(ans:str):
 
-    response  ="hellll"
+    response  = next_question(ans)
 
     return response
 
