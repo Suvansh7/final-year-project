83169fd28084eae861e9209129c8ce6d5ded229a
Suvansh7 <jindalsuvansh7@gmail.com>
2024-08-13 17:38:16 +0530

    fg

--- api/main.py
+++ api/main.py
@@ -1,32 +1,22 @@
 from fastapi import FastAPI, Depends
-import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, SystemMessage
+
 
 app = FastAPI()
 
-# Initialize the Generative AI model once at startup
-GOOGLE_API_KEY = "AIzaSyD-WsKQ2O-isAK-PJjFxTusl1-TxcQ8l2E"
-genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
-
-
-# Dependency to store and retrieve corpus
 def get_corpus():
     return {"corpus": ""}
 
 def next_question(ans: str, corpus: str) -> str:
-    p = f'''
-        Act Like an Interviewer and ask a question that should be relevant to the following answer -
-        {ans}
-
-        Make sure you ask questions that are relevant to {corpus}, you can also cross question the answer of interviewee if they contradict their own answers.
-        You can easily do this as you have all the answers of the interviewee which are - {corpus}
-
-        Dont give anything else rather that a single line question.
-        Also dont include any astricts !
-    '''
-    
-    response = model.generate_content(p)
-    ques = response.text
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", convert_system_message_to_human=True,google_api_key="AIzaSyD-WsKQ2O-isAK-PJjFxTusl1-TxcQ8l2E")
+    ans = model(
+    [
+    SystemMessage(content="Only Generate a single line question relevant to given answer"),
+    HumanMessage(content=f"Current Answer - {ans} /n You can also take refrence to previous answers which are - {corpus} "),
+    ]
+    )
+    ques = ans.content
     return ques
 
 @app.get("/")
