53dd68e8fe01d7faaaccb9670475fdd7c4b74330
Suvansh7 <jindalsuvansh7@gmail.com>
2024-08-13 17:57:58 +0530

    chchch

--- api/main.py
+++ api/main.py
@@ -13,8 +13,8 @@ def next_question(ans: str, corpus: str) -> str:
     model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", convert_system_message_to_human=True,google_api_key="AIzaSyD-WsKQ2O-isAK-PJjFxTusl1-TxcQ8l2E")
     ans = model(
     [
-    SystemMessage(content="Only Generate a single line Counter question relevant to given answer"),
-    HumanMessage(content=f"Current Answer - {ans} /n If you can't generate question from the givin answer that take refrence of previous answers which are -/n {corpus}/n, to generate the question "),
+    SystemMessage(content="Only Generate a single line next question relevant to given answer to evaluate the candidate skills, As you are taking an interview"),
+    HumanMessage(content=f"Answer - {ans} /n If answer is not worth it to generate question then take refrence of previous answers which are -/n {corpus}"),
     ]
     )
     ques = ans.content
@@ -24,6 +24,14 @@ def next_question(ans: str, corpus: str) -> str:
 def display():
     first_question = "Tell Me about Yourself?"
     return first_question
+
+
+@app.get("/check")
+def ask( state: dict = Depends(get_corpus)):
+    
+    
+    response = state["corpus"]
+    return response
 
 @app.get("/com")
 def ask(ans: str, state: dict = Depends(get_corpus)):
