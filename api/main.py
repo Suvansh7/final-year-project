acd7a110313bb66153a597975c8a39f222ea355b
Suvansh7 <96539773+Suvansh7@users.noreply.github.com>
2025-04-02 23:47:02 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,14 +2,17 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from langchain.chat_models import ChatOpenAI
 import getpass
 import os
 # from langchain_community.vectorstores import Chroma
 # from langchain_openai import OpenAIEmbeddings
 from langchain_openai import OpenAI
 
-
+a1 = "sk-proj-RfC43pSgqSUf0kCFv-B6Fami_2K3iQV"
+b2 = "LmJa5aVfGxfgvtGfrRKJTOkRZ2fdmeLZpWKIzQz"
+c3 = "QVCUT3BlbkFJY2ilRNej8fpdSNnnlvQdDdyUJqxI"
+d4 = "rL489jICVPeX4YX1b0JVrRwMqp5JHaZovtJFRVBcKH"
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -21,10 +24,9 @@ app.add_middleware(
 # model_gemini = ChatGoogleGenerativeAI(model="gemini-1.5-flash", convert_system_message_to_human=True,google_api_key="AIzaSyD-WsKQ2O-isAK-PJjFxTusl1-TxcQ8l2E")
 
 def conversation(message): 
+    OPENAI_API_KEY = a1 + b2+ c3+d4+"ldoA"
+    model = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
     
-    model = ChatOpenAI()
-    if "OPENAI_API_KEY" not in os.environ:
-        os.environ["OPENAI_API_KEY"] = getpass.getpass(os.environ["OPEN_API"])
 
 
 
